6d8eb6a013b794ae460f67b2a89e8e1da4bbd8a6
Andrew Silva <asilvaa9@gmail.com>
2018-04-18 17:56:48 -0400

    added ability to visualize games crudely

--- utils/generate_n_games_pkl.py
+++ utils/generate_n_games_pkl.py
@@ -3,10 +3,11 @@ import torchcraft
 from torchcraft import replayer
 import os
 import pickle
+import numpy as np
 import data_utils
 
 unit_attrs = ['type', 'x', 'y', 'health', 'max_health', 'shield', 'max_shield', 'energy', 'maxCD', 'groundCD',
-              'airCD', 'visible', 'type', 'armor', 'shieldArmor', 'size', 'pixel_x', 'pixel_y', 'pixel_size_x',
+              'airCD', 'visible', 'armor', 'shieldArmor', 'size', 'pixel_x', 'pixel_y', 'pixel_size_x',
               'pixel_size_y', 'groundATK', 'airATK', 'groundDmgType', 'airDmgType', 'groundRange', 'airRange',
               'velocityX', 'velocityY', 'playerId', 'resources']
 flags = ['accelerating', 'attacking', 'attack_frame', 'being_constructed', 'being_gathered', 'being_healed', 'blind', 'braking', 'burrowed', 'carrying_gas', 'carrying_minerals', 'cloaked', 'completed', 'constructing', 'defense_matrixed', 'detected', 'ensnared', 'flying', 'following', 'gathering_gas', 'gathering_minerals', 'hallucination', 'holding_position', 'idle', 'interruptible', 'invincible', 'irradiated', 'lifted', 'loaded', 'locked_down', 'maelstrommed', 'morphing', 'moving', 'parasited', 'patrolling', 'plagued', 'powered', 'repairing', 'researching', 'selected', 'sieged', 'starting_attack', 'stasised', 'stimmed', 'stuck', 'targetable', 'training', 'under_attack', 'under_dark_swarm', 'under_disruption_web', 'under_storm', 'upgrading']
@@ -16,10 +17,16 @@ flags = ['accelerating', 'attacking', 'attack_frame', 'being_constructed', 'bein
 
 # Currently not using flags (see above) or: Commands, crafted features (distance from start base, etc.)
 def unit_to_dict(unit_in, add_orders=True):
+    """
+    convert a unit to a dictionary of all attributes
+    :param unit_in: unit to interpret
+    :param add_orders: should i add orders to the dict?
+    :return: dictionary of {key: value} for all keys in unit_attrs global
+    """
     unit_dict = dict()
-    unit_dict['type_name'] = data_utils.type_to_name(unit.type)
+    unit_dict['type_name'] = data_utils.type_to_name(unit_in.type)
     for attribute in unit_attrs:
-        unit_dict[attribute] = getattr(unit, attribute)
+        unit_dict[attribute] = getattr(unit_in, attribute)
         if add_orders:
             order_list = []
             for order in unit_in.orders:
@@ -29,6 +36,11 @@ def unit_to_dict(unit_in, add_orders=True):
 
 
 def order_to_dict(order_in):
+    """
+    transform an order into a dict of attributes and values
+    :param order_in: order to interpret
+    :return: dict of {type:type, targetId:target, targetX:target, targetY:target}
+    """
     order_dict = dict()
     order_dict['type'] = order_in.type
     order_dict['targetId'] = order_in.targetId
@@ -38,6 +50,11 @@ def order_to_dict(order_in):
 
 
 def unit_to_arr(unit_in):
+    """
+    transform a unit into an (attributes, order) tuple
+    :param unit_in: unit to gather info on
+    :return: (attributes, order) tuple
+    """
     unit_array = []
     for attribute in unit_attrs:
         unit_array.append(getattr(unit_in, attribute))
@@ -48,92 +65,146 @@ def unit_to_arr(unit_in):
     return [unit_array, label]
 
 
+def unit_to_np(unit_in):
+    """
+    use unit_to_arr to get a np array of unit data
+    :param unit_in: unit to turn into np array
+    :return: np array of all unit data + their current order
+    """
+    unit_data, label = unit_to_arr(unit_in)
+    unit_data.append(label)
+    return np.array(unit_data)
+
+
+def get_nearest_units(unit_in, all_units, threshold=200, n_nearest=1):
+    """
+    return a np array of self + nearest n_nearest units
+    :param unit_in: unit to compare to
+    :param all_units: all units owned by the same player
+    :param threshold: must be at least this close
+    :param n_nearest: how many neighbors to return
+    :return: 2d np array of [units, attributes]
+    """
+    distances = []
+    candidates = None
+    invalid_types = [13, 14, 15] # [spider mine, nuke, civilian??(not even a combat unit?)]
+    for unit in all_units:
+        if unit.id == unit_in.id:
+            continue
+        # If unit is not a valid unit (buildings are > 103)
+        if unit.type in invalid_types or unit.type > 103:
+            continue
+        #TODO: make sure i'm not comparing to a building...
+        x_diff = abs(unit.pixel_x - unit_in.pixel_x)
+        y_diff = abs(unit.pixel_y - unit_in.pixel_y)
+        if x_diff > threshold or y_diff > threshold:
+            continue
+        distances.append(np.sqrt(x_diff**2+y_diff**2))
+        if candidates is None:
+            candidates = unit_to_np(unit)
+        else:
+            candidates = np.vstack((candidates, unit_to_np(unit)))
+    # there are 30 dimensions per unit
+    if candidates is None:
+        return np.zeros((n_nearest, 30))
+    elif len(distances) <= n_nearest:
+        shape_mod = n_nearest + 1 - len(distances)
+        candidates = np.vstack((candidates, np.zeros((shape_mod, 30))))
+        distances = distances + [max(distances)+1]*shape_mod
+    distances = np.array(distances)
+    nearest = np.argpartition(distances, n_nearest)
+    return candidates[nearest[:n_nearest]]
+
 # This file is generated from `get_good_replays.py`
 replays_master = open('good_files.txt', 'r').readlines()
 for i in range(len(replays_master)):
     replays_master[i] = replays_master[i].split('\n')[0]
 
 
-# this is how many games i'm going to save:
-num_games_to_parse = 3
-# Window size for action rec (in # frames, not in # sec):
-window_size = 24
-# counter to track how many i've saved:
-counter = 0
-# master list that will be pickled / saved:
-game_list = []
-# I only want information on these units [zergling, marine, scv, drone, probe, zealot]:
-valid_types = [37, 0, 7, 41, 64, 65]
-# loop over replay master list, grab first "num_games_to_parse"
-for full_filename in replays_master:
+def get_temporal_replays():
+    step_size = 8  # 1 frame per second if step size = 8
 
-    replay = replayer.load(full_filename)
-    # TODO: Remove this hack eventually, but for now there is too much data in a 13k frame game looking at 6 unit types
-    if len(replay) > 5500:
-        continue
+    for full_filename in replays_master:
 
-    if counter >= num_games_to_parse:
-        break
-    counter += 1
+        replay = replayer.load(full_filename)
 
-    print("loaded replay: " + full_filename + " of length:" + str(len(replay)))
-    # ids of units I care about:
-    for frame_number in range(len(replay)):
-        frame = replay.getFrame(frame_number)
-        units = frame.units
-        for player_id, unit_arr in units.iteritems():
-            if player_id < 0:
-                # This is a map resource / object, not an army unit
-                continue
-            # For each unit in this frame:
-            for unit in unit_arr:
-                # Make sure it's a type that I care about
-                if unit.type in valid_types:
-                    unit_data_arr = unit_to_arr(unit)
-                    # Don't add if I already have this exact state/label combo
-                    if (unit_data_arr in game_list) or (unit_data_arr[1] == 133):
-                        continue
-                    else:
-                        game_list.append(unit_data_arr)
+        print("loaded replay: " + full_filename + " of length:" + str(len(replay)))
+        units_over_time = {}
+        for frame_number in range(0, len(replay), step_size):
+            frame = replay.getFrame(frame_number)
+            units = frame.units
+            for player_id, unit_arr in units.iteritems():
+                if player_id < 0:
+                    # This is a map resource / object, not an army unit
+                    continue
+                # For each unit in this frame:
+                for unit in unit_arr:
+                    # Make sure it's a type that I care about
+                    if unit.type in valid_types:
+                        # get nearest neighbors
+                        neighbors = get_nearest_units(unit, unit_arr, threshold=200, n_nearest=11)
+                        # create matrix with me at top, my neighbors under me
+                        unit_and_neighbors = np.vstack((unit_to_np(unit), neighbors))
+                        if unit.id in units_over_time:
+                            units_over_time[unit.id].append(unit_and_neighbors)
+                        else:
+                            units_over_time[unit.id] = [unit_and_neighbors]
+                # End iteration over units for player_id in this frame
+            # End iteration over units for all players in this frame
+        new_fn = '/media/asilva/HD_home/my_sc_data/temporal_pickles/' + full_filename.split('/')[-1] + '.pkl'
+        pickle.dump(units_over_time, open(new_fn, 'wb'))
+        print('dumped to ' + new_fn)
+        # End iteration over all frames
+    return True
 
-pickle_filename = str(num_games_to_parse)+"_games.pkl"
-pickle.dump(game_list, open(pickle_filename, 'wb'))
-print("Dump complete.")
 
-# THIS IS OLD, SAVING IT FOR THE FUTURE POTENTIALLY:
-# This saves everything in a temporally continuous way. So it watches a single unit over the course of it's life.
-# Unfortunately, it's stupidly massive. I might want it later (or like..immediately), but for now I'm going to trim
-# Down the size of my data by removing duplicates. Above code is for a much smaller / less repetitive dataset
-# for full_filename in replays_master:
-#
-#     replay = replayer.load(full_filename)
-#
-#     if counter >= num_games_to_parse:
-#         break
-#     counter += 1
-#
-#     print("loaded replay: " + full_filename + " of length:" + str(len(replay)))
-#     all_frames = []
-#     # ids of units I care about:
-#     units_i_want = {}
-#     for frame_number in range(len(replay)):
-#         frame = replay.getFrame(frame_number)
-#         units = frame.units
-#         this_frame = []
-#         for player_id, unit_arr in units.iteritems():
-#             if player_id < 0:
-#                 # This is a map resource / object, not an army unit
-#                 continue
-#             # For each unit in this frame:
-#             for unit in unit_arr:
-#                 # Make sure it's a type that I care about
-#                 if unit.type in valid_types:
-#                     # If I already have it, append a new [state, label] to it, else add it to my dict
-#                     if unit.id in units_i_want:
-#                         units_i_want[unit.id].append(unit_to_arr(unit))
-#                     else:
-#                         units_i_want[unit.id] = [unit_to_arr(unit)]
-#         # append list (frame) of lists (valid units) of [state, item] tuples to my gamewide list
-#         all_frames.append(units_i_want.values())
-#     # save gamewide list to all games
-#     game_list.append(all_frames)
+def get_unique_unit_replay():
+    game_list = []
+    counter = 0
+    # loop over replay master list, grab first "num_games_to_parse"
+    for full_filename in replays_master:
+
+        replay = replayer.load(full_filename)
+
+        if counter >= num_games_to_parse:
+            break
+        counter += 1
+
+        print("loaded replay: " + full_filename + " of length:" + str(len(replay)))
+        # ids of units I care about:
+        for frame_number in range(len(replay)):
+            frame = replay.getFrame(frame_number)
+            units = frame.units
+            for player_id, unit_arr in units.iteritems():
+                if player_id < 0:
+                    # This is a map resource / object, not an army unit
+                    continue
+                # For each unit in this frame:
+                for unit in unit_arr:
+                    # Make sure it's a type that I care about
+                    if unit.type in valid_types:
+                        unit_data_arr = unit_to_arr(unit)
+                        # Don't add if I already have this exact state/label combo
+                        if (unit_data_arr in game_list) or (unit_data_arr[1] == 133):
+                            continue
+                        else:
+                            game_list.append(unit_data_arr)
+    return game_list
+
+
+if __name__ == '__main__':
+
+    # this is how many games i'm going to save:
+    num_games_to_parse = 3
+    # I only want information on these units [zergling, marine, scv, drone, probe, zealot]:
+    valid_types = [37, 0, 7, 41, 64, 65]
+
+    temporal = True
+    if temporal:
+        final_data = get_temporal_replays()
+    else:
+        final_data = get_unique_unit_replay()
+
+    pickle_filename = str(num_games_to_parse)+"_games.pkl"
+    pickle.dump(final_data, open(pickle_filename, 'wb'))
+    print("Dump complete.")
---
+++ utils/visualize_unit_movement.py
@@ -0,0 +1,185 @@
+# Created by Andrew Silva on 4/18/18
+# This file will open up an opencv frame with circles for units, one opencv frame per game frame. press 0 / whatever to
+# step through frames of the game. Default will be 1fps
+import torchcraft
+from torchcraft import replayer
+import os
+import pickle
+import numpy as np
+import data_utils
+import cv2
+
+unit_attrs = ['type', 'x', 'y', 'size', 'pixel_x', 'pixel_y', 'pixel_size_x', 'pixel_size_y', 'playerId']
+flags = ['accelerating', 'attacking', 'attack_frame', 'being_constructed', 'being_gathered', 'being_healed', 'blind', 'braking', 'burrowed', 'carrying_gas', 'carrying_minerals', 'cloaked', 'completed', 'constructing', 'defense_matrixed', 'detected', 'ensnared', 'flying', 'following', 'gathering_gas', 'gathering_minerals', 'hallucination', 'holding_position', 'idle', 'interruptible', 'invincible', 'irradiated', 'lifted', 'loaded', 'locked_down', 'maelstrommed', 'morphing', 'moving', 'parasited', 'patrolling', 'plagued', 'powered', 'repairing', 'researching', 'selected', 'sieged', 'starting_attack', 'stasised', 'stimmed', 'stuck', 'targetable', 'training', 'under_attack', 'under_dark_swarm', 'under_disruption_web', 'under_storm', 'upgrading']
+# Uncomment for flags as attributes:
+unit_attrs += flags
+
+
+# Currently not using flags (see above) or: Commands, crafted features (distance from start base, etc.)
+def unit_to_dict(unit_in, add_orders=True):
+    """
+    convert a unit to a dictionary of all attributes
+    :param unit_in: unit to interpret
+    :param add_orders: should i add orders to the dict?
+    :return: dictionary of {key: value} for all keys in unit_attrs global
+    """
+    unit_dict = dict()
+    unit_dict['type_name'] = data_utils.type_to_name(unit_in.type)
+    for attribute in unit_attrs:
+        unit_dict[attribute] = getattr(unit_in, attribute)
+        if add_orders:
+            order_list = []
+            for order in unit_in.orders:
+                order_list.append(order_to_dict(order))
+            unit_dict['orders'] = order_list
+    return unit_dict
+
+
+def order_to_dict(order_in):
+    """
+    transform an order into a dict of attributes and values
+    :param order_in: order to interpret
+    :return: dict of {type:type, targetId:target, targetX:target, targetY:target}
+    """
+    order_dict = dict()
+    order_dict['type'] = order_in.type
+    order_dict['targetId'] = order_in.targetId
+    order_dict['targetX'] = order_in.targetX
+    order_dict['targetY'] = order_in.targetY
+    return order_dict
+
+# This file is generated from `get_good_replays.py`
+replays_master = open('good_files.txt', 'r').readlines()
+for i in range(len(replays_master)):
+    replays_master[i] = replays_master[i].split('\n')[0]
+
+
+def play_opencv_replay(full_replay_path, playerid=0, framerate=1, save_video=False):
+    """
+    This will open the opencv frames, 1 per sec for stepping through a game
+    Default behavior will be to only follow player 0
+    Default behavior will also only consider workers and basic attackers.
+    :param full_replay_path: full filename for replay (for example: '/media/asilva/StarCraft/bwrep_01234.tcr')
+    :param playerid: id of player to show units for. 2 or greater for both players to be shown
+    :return: True on success I guess?
+    """
+    # TODO: change color of unit or shape or whatever if they're attacking, losing health, order changing, whatever
+    map_shape = (1024, 1024, 3)
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    out = cv2.VideoWriter('output.avi', fourcc, 20.0, (1024, 1024))
+
+    step_size = 8  # 1 frame per second if step size = 8
+    replay = replayer.load(full_replay_path)
+    wait_time = int(1000.0/framerate)
+    print("loaded replay: " + full_replay_path + " of length:" + str(len(replay)))
+    for frame_number in range(0, len(replay), step_size):
+        frame = replay.getFrame(frame_number)
+        units = frame.units
+        this_frame = {}
+        white_bg = np.ones(map_shape) * 255
+
+        for player_id, unit_arr in units.iteritems():
+            # I only want to look at the player i pass in (default 0)
+            if player_id != playerid and playerid < 2:
+                continue
+            # For each unit in this frame:
+            for unit in unit_arr:
+                # Make sure it's a type that I care about
+                if unit.type in valid_types:
+                    # save it
+                    this_frame[unit.id] = unit_to_dict(unit)
+            # End iteration over units for player_id in this frame
+        # End iteration over units for all players in this frame
+        # TODO: right here, create a white image, draw circles for each unit
+        for unit_id, unit_dict in this_frame.iteritems():
+            unit_color = color_dict[unit_dict['type']]
+            if unit_dict['attacking']:
+                unit_color = (0, 0, 255)
+            if unit_dict['playerId'] == 0:
+                unit_center = (unit_dict['x']*2, unit_dict['y']*2)
+                cv2.circle(white_bg, unit_center,
+                           unit_dict['size']*4,
+                           color=unit_color,
+                           thickness=-1)
+            else:
+                unit_top_left = (unit_dict['x'] * 2 - unit_dict['size'] * 2, unit_dict['y'] * 2 - unit_dict['size']*2)
+                unit_bot_right = (unit_dict['x'] * 2 + unit_dict['size'] * 2, unit_dict['y'] * 2 + unit_dict['size']*2)
+                cv2.rectangle(white_bg,
+                              unit_top_left,
+                              unit_bot_right,
+                              color=unit_color,
+                              thickness=-1)
+        cv2.imshow('frame', white_bg)
+        if save_video:
+            out.write(np.uint8(white_bg))
+        cv2.waitKey(wait_time)
+    # End iteration over all frames
+    out.release()
+    cv2.destroyAllWindows()
+    return True
+
+
+if __name__ == '__main__':
+
+    # this is how many games i'm going to save:
+    num_games_to_parse = 3
+    # TODO: add more units to valid types and corresponding colors
+    # I only want information on these units:
+    terran_valid_types = [0, 1, 2, 3, 5, 7, 8, 9, 11, 12, 13, 30, 32, 33, 34, 58]
+    zerg_valid_types = [37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 50, 62, 103]
+    protoss_valid_types = [60, 61, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 83, 84, 85]
+    zerg_babies = [35, 36, 59, 97]
+    valid_types = terran_valid_types + zerg_valid_types + protoss_valid_types
+
+    # map types to colors
+    color_dict = {
+        0: (0, 255, 0),
+        1: (255, 85, 0),
+        2: (85, 255, 0),
+        3: (85, 85, 0),
+        5: (255, 255, 0),
+        30: (255, 255, 0),
+        7: (255, 0, 0),
+        8: (0, 255, 85),
+        9: (0, 255, 255),
+        11: (0, 85, 255),
+        12: (255, 0, 255),
+        13: (0, 0, 0),
+        32: (15, 125, 150),
+        33: (255, 0, 85),
+        58: (85, 85, 85),
+        34: (85, 0, 85),
+        37: (255, 0, 0),
+        38: (255, 85, 0),
+        39: (85, 85, 0),
+        40: (0, 0, 0),
+        41: (0, 255, 0),
+        42: (0, 255, 255),
+        43: (0, 255, 85),
+        44: (255, 0, 255),
+        45: (255, 85, 255),
+        46: (85, 85, 85),
+        50: (0, 0, 0),
+        62: (255, 255, 85),
+        103: (85, 255, 255),
+        64: (255, 0, 0),
+        65: (255, 85, 0),
+        60: (85, 85, 0),
+        61: (0, 0, 0),
+        63: (0, 255, 0),
+        66: (0, 255, 255),
+        67: (0, 255, 85),
+        68: (255, 0, 255),
+        69: (255, 85, 255),
+        70: (85, 85, 85),
+        71: (0, 0, 0),
+        72: (255, 255, 85),
+        73: (85, 255, 255),
+        83: (0, 85, 255),
+        84: (255, 255, 0),
+        85: (15, 125, 150)
+                  }
+    fps = 20
+    replay_path = '/media/asilva/HD_home/StarData/dumped_replays/1/bwrep_zkcw9.tcr'
+    save_vid = True
+    play_opencv_replay(replay_path, playerid=2, framerate=fps, save_video=save_vid)
